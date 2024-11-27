3c9d60c342909042031186c79090d6295d9d5c43
Adarsh <adarshnd14@gmail.com>
2024-11-28 01:08:01 +0530

    package update

--- server.js
+++ server.js
@@ -40,11 +40,3 @@ app.use("/api/posts", posts);
 app.use("/api/users", users);
 app.use("/api/comments", comments);
 app.use("/api/messages", messages);
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
